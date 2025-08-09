53f2e40fca8879c1c7f6d94b6c2f4e253581fab7
demetriotorgan <demetriotorgan@hotmail.com>
2025-08-08 23:21:07 -0300

    cors01

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 const express = require('express');
 const mongoose = require('mongoose');
 require('dotenv').config();
+const cors = require('cors');
 
 const routes = require('./routes/Routes');
 
 const app = express();
+app.use(cors({
+    origin:'*',
+}));
+app.use((req,res, next)=>{
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
+    next();    
+  });
+
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
