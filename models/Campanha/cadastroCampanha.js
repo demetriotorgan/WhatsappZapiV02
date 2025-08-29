2686eaacd6f030bf1e9e1baadfa93da084f69584
demetriotorgan <demetriotorgan@hotmail.com>
2025-08-29 15:35:03 -0300

    ImpulsionamentoSchema

--- models/Campanha/cadastroCampanha.js
+++ models/Campanha/cadastroCampanha.js
@@ -1,5 +1,16 @@
 const mongoose = require('mongoose');
 
+const ImpulsionamentoSchema = new mongoose.Schema({
+    data:Date,
+    urlImagem:String,
+    mensagemEnviada: String,
+    contato:String,
+    telefoneContato:String,
+    enviados:Number,
+    tempoEnvio:String,
+    metaSelecionada:String
+});
+
 const CadastroCampanhaSchema = new mongoose.Schema({
     nomeCliente:String,
     empresa:String,
@@ -12,8 +23,10 @@ const CadastroCampanhaSchema = new mongoose.Schema({
     id_campanha:String,
     categoria:String,
     objetivos:Array,
-    metas:Array
+    metas:Array,
+    Impulsionamento:[ImpulsionamentoSchema]
 });
+
 CadastroCampanhaSchema.pre('save', function(next){
     if(!this.id_campanha){
         this.id_campanha = this._id.toString();
