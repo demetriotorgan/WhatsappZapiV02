d6c5ecd4f14df716f2a8ec7d72510fefe3b1f0f3
demetriotorgan <demetriotorgan@hotmail.com>
2025-09-02 11:09:40 -0300

    webHookSalvarContatos

--- routes/routesWebHooks.js
+++ routes/routesWebHooks.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const MensagemRecebida = require('../models/MensagemRecebida');
 
+
 require('dotenv').config();
 
 const router = express.Router();
@@ -24,3 +25,40 @@ router.post('/webhook', async(req,res)=>{
 });
 
 module.exports = router;
+
+//Get para baixar as mensagens recebidas
+router.get('/webhook-mensagens', async(req,res)=>{
+   try {
+    //Busca mensagens no banco de dados
+    const mensagens = await MensagemRecebida.find().sort({receivedAt: -1});
+
+    //Lista de telefones salvos:
+    const telefones = [];
+    mensagens.forEach(msg=>{
+        try {
+            const payload = JSON.parse(msg.rawPayload);
+            if(payload.phone){
+                telefones.push(payload.phone);
+            }
+        } catch (err) {
+            console.error('Erro ao parsear rawPayload: ', err.message);
+        }
+    });
+
+    //Remover Telefones duplicados
+    const telefonesUnicos = [...new Set(telefones)];
+
+    res.status(200).json({
+        sucesso:true,        
+        totalMensagens:mensagens.length,
+        totalTelefones: telefonesUnicos.length,
+        telefonesUnicos        
+    }); 
+   } catch (err) {
+    console.error('Erro ao buscar mensagens recebidas', err);
+    res.status(500).json({
+        sucesso:false,
+        erro:'Erro interno no servidor'
+    });
+   }
+});
